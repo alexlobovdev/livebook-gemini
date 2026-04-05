d66e75856c13d5eccb4b5ab728bca18d5577e154
alex <alexlobov.dev@gmail.com>
2026-04-05 19:51:36 +0500

    редиска4

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -5,4 +5,8 @@ mkdir -p storage/framework/{cache,sessions,views} storage/logs storage/app/temp 
 chown -R www-data:www-data storage bootstrap/cache
 chmod -R ug+rwX storage bootstrap/cache
 
+if [ "$#" -gt 0 ]; then
+  exec "$@"
+fi
+
 exec php-fpm
